6c655d71d2a039aeac18253fbb179a76b81643bf
Thien Bui <thienbui797@gmail.com>
2018-04-12 12:07:13 +1000

    dot env

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('./server/config/passport');
-var session = require('express-session');
-var routes = require('./server/routes');
-var local = require('./server/config/local.js');
-
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var passport = require("./server/config/passport");
+var session = require("express-session");
+var routes = require("./server/routes");
+var local = require("./server/config/local.js");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'server', 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "server", "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "bower_components")));
 
 //passport
-app.use(session({ secret: 'UOW booking', resave: false, saveUninitialized: true }));
+app.use(
+  session({ secret: "UOW booking", resave: false, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -33,24 +35,24 @@ app.use(passport.session());
 app.use(local);
 
 // routes
-app.use('/', routes(passport));
+app.use("/", routes(passport));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
