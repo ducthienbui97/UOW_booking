3604be1070ae838caf4782687b1592efa06e2815
Thien Bui <thienbui797@gmail.com>
2018-05-10 15:44:58 +1000

    favicon

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set("views", path.join(__dirname, "server", "views"));
 app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public','images' ,'favicon.png')));
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
